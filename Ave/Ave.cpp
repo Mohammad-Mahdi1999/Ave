ff38e448d579f750ae9229df709855bfc7cb54d1
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-07 16:21:50 +0330

    Start of Ave

---
+++ Ave/Ave.cpp
@@ -0,0 +1,19 @@
+// In the name of God !
+//Ave
+//v1.0
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a, b, c, Ave;
+	cout << "Enter three numbers :" << endl;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	Ave = (a + b + c) / 3;
+	cout << "Average of these numbers is " << Ave << "!" << endl;
+	system("pause");
+}
+//Created by /%++;; Mohammad-Mahdi Farah-bakhsh ;;++%\
